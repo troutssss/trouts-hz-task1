562624439cc3b0c2ee13062508d3c01645a20363
Hertz <hzin_@naver.com>
2019-09-19 21:04:40 +0900

    ㅇㅇ

--- app/src/main/java/com/example/hzz6/troutstask1/HzApi.kt
+++ app/src/main/java/com/example/hzz6/troutstask1/HzApi.kt
@@ -5,11 +5,14 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
+import com.google.gson.annotations.SerializedName
+import retrofit2.Call
+
 
 const val CLIENT_ID = "jJXGy7VHbKld3ZJDUFOw"
 const val CLIENT_SECRET	 = "5R9RZ0tNNm"
 
-const val BASE_URL = "https://openapi.naver.com/v1"
+const val BASE_URL = "https://openapi.naver.com/v1/"
 
 
 object HzApi {
@@ -27,8 +30,44 @@ object HzApi {
 }
 
 interface TroutsService {
-    @Headers("CLIENT_ID :  $CLIENT_ID ," +
-            "CLIENT_SECRET : $CLIENT_SECRET ")
-    @GET("/search/movie.json")
-    fun getNaverMovies(@Query("query") searchWord: String)
+    @Headers(
+        "X-Naver-Client-Id: $CLIENT_ID",
+        "X-Naver-Client-Secret: $CLIENT_SECRET"
+    )
+    @GET("search/movie.json")
+    fun getNaverMovies(@Query("query") searchWord: String): Call<MoviesApi>
 }
+
+
+
+data class MoviesApi(
+    @SerializedName("display")
+    val display: Int,
+    @SerializedName("items")
+    val items: List<Movie>,
+    @SerializedName("lastBuildDate")
+    val lastBuildDate: String,
+    @SerializedName("start")
+    val start: Int,
+    @SerializedName("total")
+    val total: Int
+)
+
+data class Movie(
+    @SerializedName("actor")
+    val actor: String,
+    @SerializedName("director")
+    val director: String,
+    @SerializedName("image")
+    val image: String,
+    @SerializedName("link")
+    val link: String,
+    @SerializedName("pubDate")
+    val pubDate: String,
+    @SerializedName("subtitle")
+    val subtitle: String,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("userRating")
+    val userRating: String
+)
--- app/src/main/java/com/example/hzz6/troutstask1/MainActivity.kt
+++ app/src/main/java/com/example/hzz6/troutstask1/MainActivity.kt
@@ -1,18 +1,35 @@
 package com.example.hzz6.troutstask1
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        HzApi.troutsService.getNaverMovies("토르")
+            .enqueue(object: Callback<MoviesApi> {
+                override fun onFailure(call: Call<MoviesApi>, t: Throwable) {
+
+                }
+
+                override fun onResponse(call: Call<MoviesApi>, response: Response<MoviesApi>) {
+                    Log.i(TAG, "${response.body()}")
+                }
+            })
     }
 
 }
